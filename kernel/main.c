4061c640c62b7a8f85b109543a57161d18e9e5f1
kizill_top_jr <kizilltopjr@gmail.com>
2026-09-24 15:52:07 +0000

    x86_64: read memory map from Limine, 510 MB usable

--- kernel/main.c
+++ kernel/main.c
@@ -10,6 +10,10 @@
     0xc7b1dd30df4c8b88, 0x0a82e883a194f07b, \
     0x9d5827dcd881dd75, 0xa3148604f6fab11b \
 }
+#define LIMINE_MEMMAP_REQUEST { \
+    0xc7b1dd30df4c8b88, 0x0a82e883a194f07b, \
+    0x67cf3d9d378a806f, 0xe304acdfc50c3c62 \
+}
 #define LIMINE_BASE_REVISION(n) { \
     0xf9562b2d5c95a6c8, 0x6a7b384944536bdc, (n) \
 }
@@ -42,6 +46,34 @@ struct limine_framebuffer_request {
     struct limine_framebuffer_response *response;
 };
 
+struct limine_memmap_entry {
+    u64 base;
+    u64 length;
+    u64 type;
+};
+
+struct limine_memmap_response {
+    u64 revision;
+    u64 entry_count;
+    struct limine_memmap_entry **entries;
+};
+
+struct limine_memmap_request {
+    u64 id[4];
+    u64 revision;
+    struct limine_memmap_response *response;
+};
+
+// memmap entry types
+#define MEMMAP_USABLE                0
+#define MEMMAP_RESERVED              1
+#define MEMMAP_ACPI_RECLAIMABLE      2
+#define MEMMAP_ACPI_NVS              3
+#define MEMMAP_BAD_MEMORY            4
+#define MEMMAP_BOOTLOADER_RECLAIMABLE 5
+#define MEMMAP_KERNEL_AND_MODULES    6
+#define MEMMAP_FRAMEBUFFER           7
+
 __attribute__((section(".limine_requests_start"), used))
 static volatile u64 limine_requests_start_marker[4] = LIMINE_REQUESTS_START_MARKER;
 
@@ -51,6 +83,11 @@ static volatile u64 limine_base_revision[3] = LIMINE_BASE_REVISION(3);
 __attribute__((section(".limine_requests"), used))
 static volatile struct limine_framebuffer_request framebuffer_request = {
     .id = LIMINE_FRAMEBUFFER_REQUEST, .revision = 0, .response = 0
+};
+
+__attribute__((section(".limine_requests"), used))
+static volatile struct limine_memmap_request memmap_request = {
+    .id = LIMINE_MEMMAP_REQUEST, .revision = 0, .response = 0
 };
 
 __attribute__((section(".limine_requests_end"), used))
@@ -115,6 +152,35 @@ void kmain(void) {
     printk_color("Kizill_OS v0.3 x86_64\n", FB_GREEN);
     printk("fb:  "); printk_dec(fb->width); printk("x");
     printk_dec(fb->height); printk(" "); printk_dec(fb->bpp); printk("bpp\n\n");
+
+    // --- memory map dump ---
+    if (memmap_request.response) {
+        struct limine_memmap_response *mm = memmap_request.response;
+        u64 total_usable = 0;
+
+        printk_color("memmap:\n", FB_YELLOW);
+        printk("entries: "); printk_dec(mm->entry_count); printk("\n");
+
+        for (u64 i = 0; i < mm->entry_count; i++) {
+            struct limine_memmap_entry *e = mm->entries[i];
+
+            if (e->type == MEMMAP_USABLE) {
+                total_usable += e->length;
+            }
+
+            // print: base, length, type
+            printk("  base="); printk_hex(e->base);
+            printk(" len=");  printk_hex(e->length);
+            printk(" type="); printk_dec(e->type);
+            printk("\n");
+        }
+
+        printk_color("total usable: ", FB_GREEN);
+        printk_dec(total_usable / 1024 / 1024);
+        printk(" MB\n\n");
+    } else {
+        printk_color("memmap: no response\n", FB_RED);
+    }
 
     gdt_init();
     idt_init();
